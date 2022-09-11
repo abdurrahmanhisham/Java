be11ab87fcb3f69d6104b31e61d9155a3e960bf4
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-11 13:23:56 +0600

    Update 11-9-22.java

--- 11-9-22.java
+++ 11-9-22.java
@@ -46,3 +46,21 @@ public static void main(String[]args) {
 
 name,id,dept
 
+
+import java.util.Scanner;
+public class Test{
+	public static void main(String[]args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the size of the array:-");
+		int size=input.nextInt();
+		int[]arr=new int [size];
+		System.out.println("Enter the value of the array:-");
+		for(int i=0;i<size;i++) {
+			arr[i]=input.nextInt();
+			for(i=0;i<size;i++) {
+				System.out.println("Enter the value of array["+i+"]:-"+arr[i]);
+			}
+		}
+	}
+}
+
