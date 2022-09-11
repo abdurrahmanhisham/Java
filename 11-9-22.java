e1b762b9fc2e35d73cbcdbc96fd146e0e378ce22
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-11 12:22:37 +0600

    Update 11-9-22.java

--- 11-9-22.java
+++ 11-9-22.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+//scanner Sum
+
 import java.util.Scanner;
 public class Sum {
 public static void main(String[]args) {
@@ -8,5 +9,21 @@ public static void main(String[]args) {
 	System.out.println("Enter the second number:-");
 	int b=input.nextInt();
 	System.out.println("The sum of two numbers:-"+(a+b));
+}
+}
+
+//scanner product
+
+import java.util.Scanner;
+public class Sum {
+public static void main(String[]args) {
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter the first number:-");
+	double a=input.nextDouble();
+	System.out.println("Enter the second number:-");
+	double b=input.nextDouble();
+	System.out.println("Enter the third number:-");
+	double c=input.nextDouble();
+	System.out.println("The product of three numbers:-"+(a*b*c));
 }
 }
