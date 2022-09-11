e87b04ef19ad9f3a7a3b30a90093813a74db1e52
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-11 12:16:06 +0600

    Create 11-9-22.java

---
+++ 11-9-22.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class Sum {
+public static void main(String[]args) {
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter the first number:-");
+	int a=input.nextInt();
+	System.out.println("Enter the second number:-");
+	int b=input.nextInt();
+	System.out.println("The sum of two numbers:-"+(a+b));
+}
+}
