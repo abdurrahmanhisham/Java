c6af4466488ea32b173e4d8a0fa085cc45adf129
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-12 12:05:29 +0600

    Update 11-9-22.java

--- 11-9-22.java
+++ 11-9-22.java
@@ -1,4 +1,4 @@
-                                                       //scanner 
+                                                       //scanner SUM
 
 import java.util.Scanner;
 public class Sum {
@@ -51,22 +51,3 @@ public class test {
 
     }
 }
-
-
-import java.util.Scanner;
-public class Test{
-	public static void main(String[]args) {
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter the size of the array:-");
-		int size=input.nextInt();
-		int[]arr=new int [size];
-		System.out.println("Enter the value of the array:-");
-		for(int i=0;i<size;i++) {
-			arr[i]=input.nextInt();
-			for(i=0;i<size;i++) {
-				System.out.println("Enter the value of array["+i+"]:-"+arr[i]);
-			}
-		}
-	}
-}
-
