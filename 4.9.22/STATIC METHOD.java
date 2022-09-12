e484db7d2c1b19f6f5b46942e07c4408d9a334c3
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-12 13:45:07 +0600

    Create STATIC METHOD.java

---
+++ 4.9.22/STATIC METHOD.java
@@ -0,0 +1,18 @@
+public class test {
+
+	public static void main(String[] args) {
+		Student ob=new Student();
+		ob.show(); //for non-static method object needs to be created first
+		Student.display(); //static metods can be directly called with class
+	}
+}
+
+class Student {
+
+	void show() {
+		System.out.println("It is a non-static method");
+	}
+	static void display() {
+		System.out.println("It is a static Method");
+	}
+}
