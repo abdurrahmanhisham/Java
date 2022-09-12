a7f29e18352a6809b2f1b2cf0f3c6415e766a7a8
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-12 12:08:58 +0600

    Update 11-9.22..java

--- 11-9.22..java
+++ 11-9.22..java
@@ -17,3 +17,16 @@ public class Test{
 	}
 }
 
+
+                                        //ARRAY INITIALIZE WITHOUT SIZE
+
+public class test {
+    public static void main(String[]args)
+    {
+        int []arr={10,20,30,40,50};
+        for (int i : arr) //for(i=0;i<arr.length;i++)
+        {
+            System.out.println("The value of array: " + i); //+arr[i]
+        }
+    }
+}
