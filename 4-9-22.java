c6044b68285786b22e7bb577d3b608854bdab4bd
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-12 11:56:07 +0600

    Update 4-9-22.java

--- 4-9-22.java
+++ 4-9-22.java
@@ -1,21 +1,4 @@
-public class Student {
- String name,id;
- int age;
- static String varsity_name="DIU";
- Student(String n,String i,int a){
-	 name=n;
-	 id=i;
-	 age=a;
-	 
- }
-void show() {
-	System.out.println("Name:- "+name);
-	System.out.println("ID:- "+id);
-	System.out.println("Age:- "+age);
-	System.out.println("Varsity:- "+varsity_name);
-}
-}
-
+                                                                 //STATIC BLOCK
 
 public class Test {
 
@@ -27,7 +10,22 @@ public class Test {
 	}
 }
 
-
+public class Student {
+ String name,id;
+ int age;
+ static String varsity_name="DIU";
+ Student(String n,String i,int a){
+	 name=n;
+	 id=i;
+	 age=a;	 
+ }
+void show() {
+	System.out.println("Name:- "+name);
+	System.out.println("ID:- "+id);
+	System.out.println("Age:- "+age);
+	System.out.println("Varsity:- "+varsity_name);
+}
+}
 
 ............................................................................................................................
 
@@ -46,69 +44,54 @@ public class Test {
 	}
 }
 
+                                                     //STATIC METHOD
 
+public class test {
 
+	public static void main(String[] args) {
+		Student ob=new Student();
+		ob.show(); //for non-static method object needs to be created first
+		Student.display(); //static metods can be directly called with class
+	}
+}
 
-..............................................................
+class Student {
 
-public class Student {
- 
 	void show() {
-		System.out.println("It is Non static method");
+		System.out.println("It is a non-static method");
 	}
 	static void display() {
-System.out.println("It is static method");
-
+		System.out.println("It is a static Method");
 	}
 }
 
 
+                                                      //STATIC RESTRICTION
 
 
-public class Test {
+public class test {
 
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		Student ob=new Student();
 		ob.show();
 		Student.display();
 	}
 }
 
-
-...............................................................................................
-
-
-
-public class Student {
- int a=5;
- static int b=8;
- void show() {
-	 display();
-	 System.out.println(a);
-	 System.out.println(b);
- }
- static void display() {
-	 System.out.println("Static method.");
-	 System.out.println(b);
-	 
- }
-}
-
-
-
-
-
-
-public class Test {
-
-	public static void main(String[]args) {
-Student ob=new Student();
-ob.show();
-Student.display();
+class Student {
+	int a=5;
+	static int b=8;
+	void show() { //non-static methods can be used with both static and non-static stuffs
+		display();
+		System.out.println("Non-Static Method");
+		System.out.println(a);
+		System.out.println(b);
+	}
+	static void display() { //can not add anything non-staic i.e can't print a here.
+		System.out.println("Static Method");
+		System.out.println(b);
 	}
 }
-
-
 
 
 
@@ -116,25 +99,18 @@ Student.display();
 public class Student {
  static int id;
  static String name;
- 
  static void display() {
 	 System.out.println("ID:"+id);
 	 System.out.println("Name: "+name);
  }
-
-
  static {
 	 id=123;
 	 name="Hisham";
-			 System.out.println("Hello World");
+	 System.out.println("Hello World");
  }
 }
  
-
-
-
 public class Test {
-
 	public static void main(String[]args) {
 Student.display();
 	}
