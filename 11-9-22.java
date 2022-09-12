0e5781e0c76627e81cd5732ac6956addfdda10e8
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-12 11:59:35 +0600

    Update 11-9-22.java

--- 11-9-22.java
+++ 11-9-22.java
@@ -28,23 +28,6 @@ public static void main(String[]args) {
 }
 }
 
-                                                   //MATH
-
-public class Test {
-public static void main(String[]args) {
-	System.out.println(Math.sqrt(16));
-	System.out.println(Math.abs(-40.2));
-	System.out.println(Math.pow(3,3));
-	System.out.println(Math.PI);
-	System.out.println(Math.min(4,2));
-	System.out.println(Math.max(4,2));
-	System.out.println(Math.floor(2.6));
-	System.out.println(Math.ceil(2.4));
-	System.out.println(Math.exp(1));
-	System.out.println(Math.round(4.2));
-	System.out.println(Math.random());
-}
-}
 
                                            //SCAN NAME,ID,DEPT
 
