07c5616bda0016169cba8c832a6fa190c0f2da93
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-14 16:28:39 +0600

    Update ARRAY.java

--- 11-9-22/ARRAY.java
+++ 11-9-22/ARRAY.java
@@ -1,21 +1,25 @@
                                          //TAKE INPUT IN ARRAY
 
 import java.util.Scanner;
-public class Test{
-	public static void main(String[]args) {
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter the size of the array:-");
-		int size=input.nextInt();
-		int[]arr=new int [size];
-		System.out.println("Enter the value of the array:-");
-		for(int i=0;i<size;i++) {
-			arr[i]=input.nextInt();
-			for(i=0;i<size;i++) {
-				System.out.println("Enter the value of array["+i+"]:-"+arr[i]);
-			}
-		}
-	}
+public class Test {
+    public static void main(String[]args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = input.nextInt();
+        int []arr=new int[size];
+        System.out.println("Enter the values of the array: ");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=input.nextInt();
+        }
+        for(int i=0;i<size;i++)
+        {
+            System.out.println("The value of the array ["+i+"]: "+arr[i]);
+        }
+    }
 }
+
 
 
 
