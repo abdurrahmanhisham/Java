43fa51dc9d62256208e04a9a0de5d6768d19b71c
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-09-26 20:08:17 +0600

    Update 25-9-22.java

--- 25-9-22.java
+++ 25-9-22.java
@@ -9,3 +9,22 @@ public static void main(String args[]) {
 	System.out.println("bonus of Programmeris:"+p.bonus);
 }
 }
+
+................................................................
+	public class teacher {
+    String designation = "Lecturer", uniName="DIU";
+    public void job(){
+        System.out.println("Teaching");
+    }
+}
+class cseTeacher extends teacher{
+    String mainSubject = "CSE";
+
+    public static void main(String[]args){
+        cseTeacher obj=new cseTeacher();
+        System.out.println(obj.uniName);
+        System.out.println(obj.designation);
+        System.out.println(obj.mainSubject);
+        obj.job();
+    }
+}
