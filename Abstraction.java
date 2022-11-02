2219db6a0d85ab860b41d77bef7d0a594a0837b7
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-11-02 22:24:36 +0600

    Update Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -15,3 +15,23 @@ public class car extends vehicle {
 abstract class vehicle{
     public abstract void move();
 }
+
+
+
+
+public class car2 extends vehicle{
+    public void move(){
+        System.out.println("Car moves faster");
+    }
+    public static void main(String[]args){
+        car2 c1=new car2();
+        c1.move();
+        c1.carry();
+    }
+}
+abstract class vehicle{
+    public abstract void move();
+    public void carry(){
+        System.out.println("All vehicle carry loads");
+    }
+}
