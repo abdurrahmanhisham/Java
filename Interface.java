4aacd0e2441aa111c9981cfadb519f7eee03a149
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-11-13 14:00:31 +0600

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,29 @@
+public interface A {
+public void M();
+}
+
+..................
+  
+  public interface B {
+public void M();
+}
+
+
+.............
+  
+  public class C implements A,B{
+public void M() {
+System.out.println("okey");
+}
+}
+
+.................
+  
+  
+public class test extends C{
+	public static void main(String[]args) {
+		C obj=new C();
+		obj.M();
+	}
+
+}
