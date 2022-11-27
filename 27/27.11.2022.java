699341d5df6e8d3cc79aa32b8cc9919b9032fa09
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-11-27 13:41:54 +0600

    Update 27.11.2022.java

--- 27/27.11.2022.java
+++ 27/27.11.2022.java
@@ -82,3 +82,18 @@ public static void main(String[]args) {
 }
 }
 
+
+package gggg;
+
+public class PCA {
+static void vd(int age) {
+	if(age<18)
+		throw new ArithmeticException("not valid");
+	else
+			System.out.println("welcome to vote");
+	}
+public static void main(String[]args) {
+	vd(13);
+	System.out.println("rest of the code");
+}
+	}
