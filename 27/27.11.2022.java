8c3e6bbb9d1edadc2bcabf7ef21db9367a291992
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-11-27 12:30:29 +0600

    Create 27.11.2022.java

---
+++ 27/27.11.2022.java
@@ -0,0 +1,12 @@
+package GGG;
+public class PCA {
+public static void main(String[]args) {
+	try {
+	int b=50/0;
+}
+	catch(ArithmeticException e) {
+		System.out.println(e);
+	}
+	System.out.println("Hi");
+}
+}
