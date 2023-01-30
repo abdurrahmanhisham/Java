d7fe98f926cff06446ad7ed45e9975bc81ae7a3b
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2023-01-30 13:32:19 +0600

    Update ALL.java

--- ALL.java
+++ ALL.java
@@ -258,3 +258,27 @@ int main(){
     scanf("%d",&n);
     printf("%d",sum(n));
 }
+/*
+#include<stdio.h>
+void aaa(int n){
+    if(n%2)
+    printf("odd\n");
+    else
+    printf("even\n");
+}
+    int main(){
+        int a;
+        scanf("%d",&a);
+        aaa(a);
+}
+*/
+#include<stdio.h>
+int sum(int n){
+if(n==1) return 1;
+return n+sum(n-1);
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",sum(n));
+}
